2364d196c7136ad26168910d66d911372dbefe33
Christian Sifuentes <me@radixpoint.dev>
2022-07-18 00:09:42 -0400

    Adding part of Home section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper-info'>
+      <SubHeading title='Chase the new flavor!'/>
+      <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+    </div>
+
+    <div className='app__wrapper-img'>
+    </div>
   </div>
 );
 
