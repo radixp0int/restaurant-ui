0e4740a42ad62b73e8cbb654f48481f76fb8aa67
Christian Sifuentes <me@radixpoint.dev>
2022-07-19 23:30:49 -0400

    Adding About Us section for Web view

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,9 +9,16 @@ const Header = () => (
     <div className='app__wrapper-info'>
       <SubHeading title='Chase the new flavor!'/>
       <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+      <p className='p__opensans' style={{ margin: '2rem 0'}}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+      <button type='button' className='custom__button'>
+        Explore Menu
+      </button>
     </div>
 
     <div className='app__wrapper-img'>
+      <img src={images.welcome} alt='header img'/>
     </div>
   </div>
 );
