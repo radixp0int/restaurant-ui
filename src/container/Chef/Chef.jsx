2673fc502aa6c66aac9b84f40574e2b58d56564f
Christian Sifuentes <me@radixpoint.dev>
2022-07-31 23:46:52 -0400

    Adding About Chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
-  </div>
+	<div className='app__bg app__wrapper section__padding'>
+		<div className='app__wrapper_img app__wrapper_img-reverse'>
+			<img src={images.chef} alt='chef' />
+		</div>
+
+		<div className='app__wrapper_info'>
+			<SubHeading title="About Chef" />
+			<h1 className='headtext__cormorant'>What We Believe In</h1>
+
+			<div className='app__chef-content'>
+				<div className='app__chef-content_quote'>
+					<img src={images.quote} alt='quote' />
+					<p className='p__opensans'>QUOTE</p>
+				</div>
+				<p className='p__opensans'>QUOTE Cont.</p>
+			</div>
+
+			<div className='app__chef-sign'>
+				<p>Chef Name</p>
+				<p className='p__opensans'>Chef Title</p>
+				<img src={images.sign} alt='sign' />
+				<div />
+			</div>
+		</div>
+	</div>
 );
 
 export default Chef;
