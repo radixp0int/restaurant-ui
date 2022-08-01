78ff930d9d2fe6a965908c3aadaab7212f6e200e
Christian Sifuentes <me@radixpoint.dev>
2022-08-01 01:34:50 -0400

    Adding Awards section

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+
+import { meal } from '../../constants';
 
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const videoRef = React.useRef();
+  const handlePlay = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo)
+
+    if (playVideo) {
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+  };
+
+  return (
+    <div className='app__video'>
+      <video
+        src={meal}
+        ref={videoRef}
+        type='video/mp4'
+        loop
+        controls={false}
+        muted
+      />
+      <div className='app__video-overlay flex__center'>
+        <div
+          className={playVideo ? 'flex__center' : 'app__video-overlay_circle flex__center'}
+          onClick={handlePlay}
+        >
+          {playVideo
+            ? <BsPauseFill color='#fff' fontSize={75} />
+            : <BsFillPlayFill color='#fff' fontSize={75} />
+          }
+        </div>
+      </div>
+    </div>
+  )
+};
 
 export default Intro;
